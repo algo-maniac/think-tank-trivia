f87b1578cffad33b3096847ab88d6cec20438019
Tsaha11 <tuhin727066@gmail.com>
2023-10-05 21:44:14 +0530

    Form Preview Added

---
+++ src/components/Question.js
@@ -0,0 +1,15 @@
+import './Question.css'
+const Question=()=>{    
+    return<>
+        <div className={"questionHeader"}>
+        <div className="question">
+            <p>Explain Dandi March?</p>
+        </div>
+        <hr></hr>
+        <div className="answer">
+            <textarea readOnly cols={100} rows={3} placeholder='Write your answer'></textarea>
+        </div>
+    </div>
+    </>
+}
+export default Question
