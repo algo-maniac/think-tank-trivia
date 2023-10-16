706e893d5061e77180b22de0a2f6ebd05a73f368
Tonmoy Biswas <tonmoybiswas19122002@gamil.com>
2023-10-16 15:14:05 +0530

    username bugfix and logout added

--- src/app/dashboard/page.js
+++ src/app/dashboard/page.js
@@ -9,80 +9,81 @@ import { useEffect, useState } from "react";
 import UserContext from "@/context/userContext/userContext";
 import { useContext } from "react";
 import Loader from "@/components/Loader";
+import { signOut } from "next-auth/react";
 export default function Dashboard() {
-  const email="tuhin727066@gmail.com";
-  const [loaderFlag,setLoader]=useState(true);
-  let [data,setData]=useState([]);
-  const {user}=useContext(UserContext);
-  useEffect(()=>{
-    const fetchDetails=()=>{
-      fetch("/api/dashboard",{//automatically make a call on current domain
-        method:'POST',
-        body:JSON.stringify({user_id:user._id}),
-        headers:{
-          'Content-Type':'application/json'
+  const email = "tuhin727066@gmail.com";
+  const [loaderFlag, setLoader] = useState(true);
+  let [data, setData] = useState([]);
+  const { user } = useContext(UserContext);
+  useEffect(() => {
+    const fetchDetails = () => {
+      fetch("/api/dashboard", {//automatically make a call on current domain
+        method: 'POST',
+        body: JSON.stringify({ user_id: user._id }),
+        headers: {
+          'Content-Type': 'application/json'
         }
-      }).then((data)=>{
+      }).then((data) => {
         return data.json();
-      }).then((data)=>{
+      }).then((data) => {
         console.log(data)
         setData(data.formsList);
         setLoader(false)
-      }).catch((er)=>{
+      }).catch((er) => {
         console.log('Error');
       })
     }
     // name,date,responses.length,formid
     fetchDetails();
-  },[])
+  }, [])
   return <>
-  <div className="main">
-    <div className="left-part">
-      <div className="logo">
-        <h1 className="heading">ThinkTankTrivia</h1>
-        <div className="links">
-          <Link href={'/dashboard'}>
-            <div className="sidelinks">
-              Dashboard
-            </div>
-          </Link>
-          <Link href={'/search-form'}>
-          <div className="sidelinks">
-            Search-Form
+    <div className="main">
+      <div className="left-part">
+        <div className="logo">
+          <h1 className="heading">ThinkTankTrivia</h1>
+          <div className="links">
+            <Link href={'/dashboard'}>
+              <div className="sidelinks">
+                Dashboard
+              </div>
+            </Link>
+            <Link href={'/search-form'}>
+              <div className="sidelinks">
+                Search-Form
+              </div>
+            </Link>
+            <Link href={'/analytics'}>
+              <div className="sidelinks">
+                Analytics
+              </div>
+            </Link>
           </div>
-          </Link>
-          <Link href={'/analytics'}>
-          <div className="sidelinks">
-            Analytics
+        </div>
+        <div className="user">
+          <div className="avatar">
+            <img src={user.avatar} alt={user.name} height={30} width={50} />
           </div>
-          </Link>
+          <div className="info">
+            <span className="name">{user.username}</span>
+            <span className="logout" onClick={() => { signOut() }}>Logout</span>
+          </div>
         </div>
       </div>
-      <div className="user">
-        <div className="avatar">
-        <img src={user.avatar} alt={user.name} height={30} width={50} />
+      <div className="right-part">
+        <div className="header">
+          <div className="content">Home</div>
+          <div className="buttons">
+            <button className="button-27" role="button"><Link href={'dashboard/create-form'}>+Create Form</Link></button>
+            <button className="button-26" role="button"><Link href={'dashboard/create-quiz'}>+Create Quiz</Link></button>
+          </div>
         </div>
-        <div className="info"> 
-          <span className="name">Tuhin Saha</span>
-          <span className="logout">Logout</span>
-        </div>
-      </div>
-    </div>
-    <div className="right-part">
-      <div className="header">
-        <div className="content">Home</div>
-        <div className="buttons">
-        <button className="button-27" role="button"><Link href={'dashboard/create-form'}>+Create Form</Link></button>
-        <button className="button-26" role="button"><Link href={'dashboard/create-quiz'}>+Create Quiz</Link></button>
-        </div>
-      </div>
-      <div className="forms-container">
+        <div className="forms-container">
           <div className="sorting">
             All Forms
           </div>
           {!loaderFlag && <div className="container">
             {
-              data.map(function(val){
+              data.map(function (val) {
                 return <ResponseCard data={val} key={val._id}></ResponseCard>
               })
             }
@@ -90,8 +91,8 @@ export default function Dashboard() {
           {loaderFlag && <div>
             <Loader></Loader>
           </div>}
+        </div>
       </div>
     </div>
-  </div>
   </>
 }
--- src/app/dummy-logout/page.js
+++ src/app/dummy-logout/page.js
@@ -1,5 +1,5 @@
 "use client"
-import React,{useContext} from 'react'
+import React, { useContext } from 'react'
 import { useRouter } from 'next/navigation'
 import { useSession, signOut } from 'next-auth/react'
 import "./logout.css"
@@ -8,7 +8,7 @@ import UserContext from '@/context/userContext/userContext'
 export default function page() {
   const router = useRouter();
 
-  const {auth_status,auth_session}=useContext(UserContext);
+  const { auth_status, auth_session } = useContext(UserContext);
 
   if (auth_status === 'loading') {
     return <p className='text-center p-4 bg-slate-400'>Loading...</p>
