b43d33ba3e00299789c1db6b208956e4a8747fae
Tonmoy Biswas <tonmoybiswas19122002@gamil.com>
2023-10-19 11:21:36 +0530

    /api/analytics/user/[user_id]/[days]/ modified to return list date wise

--- src/app/api/analytics/user/[user_id]/[days]/route.js
+++ src/app/api/analytics/user/[user_id]/[days]/route.js
@@ -15,7 +15,21 @@ export async function GET(req,{params}){
             return NextResponse.json({ok:false,message:"No user found"},{status:400});
         }
         const formList=await Forms.find({$and:[{owner:{$eq:user_id}},{date:{$gt:curDate}}]},{date:1});
-        return NextResponse.json({ok:true,message:"Forms details fetched",data:{forms_no:userDoc.forms_no,forms:formList}},{status:200});
+        let list=[];
+        for(let i=parseInt(days)-1;i>=0;i--){
+            let curDate=new Date(Date.now());
+            curDate.setDate(curDate.getDate()-i);
+            let date_str=curDate.toDateString();
+            let tempList=formList.filter((doc)=>{
+                // console.log(doc.date);
+                // let tempDate=new Date(doc.date);
+                let tempDate=doc.date;
+                let tempDateStr=tempDate.toDateString();
+                return tempDateStr===date_str;
+            })
+            list.push({date_str:date_str,form_list:tempList});
+        }
+        return NextResponse.json({ok:true,message:"Forms details fetched",data:{forms_no:userDoc.forms_no,forms:list}},{status:200});
     }
     catch(err){
         return NextResponse.json({ok:false,message:err.message},{status:500});
