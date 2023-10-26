6d4851e94ead209f1b76bc907f9a4544cde592cb
Tonmoy Biswas <tonmoybiswas19122002@gamil.com>
2023-10-26 22:03:23 +0530

    bug fix in fetch response details api

--- src/app/api/fetch-response-details/[resId]/route.js
+++ src/app/api/fetch-response-details/[resId]/route.js
@@ -17,18 +17,27 @@ export async function GET(req,{params}){
         })
         .populate({
             path:'form',
-            select:"form_name "
+            select:"form_name owner",
+            populate:{
+                path:'owner',
+                select:"email"
+            }
         })
         .populate({
             path:'responses.ques_id',
             select:"-owner "
         });
         const session=await getServerSession();
-        if(session.user.email!=resDoc.user.email){
+        if(!session){
+            return NextResponse.json({ok:false,message:"The user is not authenticated"},{status:400});
+        }
+        if(session.user.email==resDoc.user.email || session.user.email==resDoc.form.owner.email){
+            return NextResponse.json({ok:true,message:"Response list fetched successfully",response_details:resDoc},{status:200});
+        }
+        // if(session.user.email!=resDoc.user.email){
             let newDoc={...resDoc,responses:[]}
             return NextResponse.json({ok:true,message:"Response list fetched successfully",response_details:newDoc},{status:200});
-        }
-        return NextResponse.json({ok:true,message:"Response list fetched successfully",response_details:resDoc},{status:200});
+        // }
     }
     catch(err){
         return NextResponse.json({ok:false,message:err.message},{status:500});
