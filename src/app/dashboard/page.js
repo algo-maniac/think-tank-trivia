4ebb407a1a29be749a1c9a71141b7508d0c8508e
Tsaha11 <tuhin727066@gmail.com>
2023-11-12 03:26:51 +0530

    Dark mode for dashboard completed

--- src/app/dashboard/page.js
+++ src/app/dashboard/page.js
@@ -3,6 +3,7 @@ import Image from "next/image";
 import Link from "next/link";
 import QuizCard from "@/components/QuizCard";
 import style from './style.module.css'
+import dark from './dark.module.css'
 import { Avatar } from "@mui/material";
 import ResponseCard from "@/components/ResponseCard";
 import { useEffect, useState } from "react";
@@ -16,7 +17,10 @@ import Card from "@/components/Card";
 import { MdDashboardCustomize } from "react-icons/md"
 import { AiOutlineFileSearch } from "react-icons/ai"
 import { IoMdAnalytics } from "react-icons/io"
+import { FaLightbulb } from "react-icons/fa";
+
 export default function Dashboard() {
+  const [mode,setMode]=useState(true)
   const [loaderFlag, setLoader] = useState(true);
   const [modal, setModal] = useState(false);
   const [error, setError] = useState(false);
@@ -25,6 +29,40 @@ export default function Dashboard() {
   let [response, setResponse] = useState([]);
   let [all_form, setAllForm] = useState(true);
   let [all_response, setAllResponse] = useState(false);
+  useEffect(()=>{
+    const modes=()=>{
+      const g=localStorage.getItem('website_mode');
+      if(g===null){
+        setMode(true);
+        localStorage.setItem('website_mode',true);
+      }
+      else{
+        let f=true;
+        if(g==='false'){
+          f=false;
+        }
+        else{
+          f=true
+        }
+        setMode(f)
+      }
+    }
+    modes()
+    setAllForm(!all_form);
+    setAllForm(!all_form);
+    setAllResponse(!all_response)
+    setAllResponse(!all_response)
+  },[])
+  const modeHandler=()=>{
+    if(mode===true){
+      setMode(false);
+      localStorage.setItem('website_mode',false);
+    }      
+    else{
+      setMode(true);
+      localStorage.setItem('website_mode',true);
+    }
+  }
   const { user, auth_session, auth_status } = useContext(UserContext);
   const router = useRouter();
   if (auth_status == 'unauthenticated') {
@@ -78,69 +116,39 @@ export default function Dashboard() {
   return <>
     {modal && <Modal val={{ type: "success", msg: "All fetched successfully" }}></Modal>}
     {error && <Modal val={{ type: "error", msg: "Failed from Server Side" }}></Modal>}
-    <div className={style.main}>
-      <div className={style.leftpart}>
-        {/* <div className={style.logo}>
-          <h1 className={style.heading}>ThinkTankTrivia</h1>
-          <div className={style.links}>
-            <Link href={'/dashboard'} className={style.anchor}>
-              <div className={style.sidelinks}>
-                Dashboard
-              </div>
-            </Link>
-            <Link href={'/search-form'} className={style.anchor}>
-              <div className={style.sidelinks}>
-                Search-Form
-              </div>
-            </Link>
-            <Link href={'/analytics'} className={style.anchor}>
-              <div className={style.sidelinks}>
-                Analytics
-              </div>
-            </Link>
-          </div>
-        </div>
-        <div className={style.user}>
-          <div className={style.avatar}>
-            <img src={auth_session.user.image} alt={user.name} height={30} width={50} />
-          </div>
-          <div className={style.info}>
-            <span className={style.name}>{user.username}</span>
-            <span className={style.logout} onClick={() => { signOut() }}>Logout</span>
-          </div>
-        </div> */}
-
-        <div className={style.upper}>
-          <div className={style.logo_icon}>
+    <div className={`${mode?style.main:dark.main}`}>
+      <div className={`${mode?style.leftpart:dark.leftpart}`}>
+        <div className={`${mode?style.upper:dark.upper}`}>
+          <div className={`${mode?style.logo_icon:dark.logo_icon}`}>
             <Link href={"/"}><img src="./favicon.png" alt="loading" /></Link>
           </div>
-          <div className={style.logo_name}>
+          <div className={`${mode?style.logo_name:dark.logo_name}`}>
             <h3>Think-Fast-Trivia</h3>
           </div>
         </div>
-        <div className={style.middle}>
-          <div className={style.child}>
+        <div className={`${mode?style.middle:dark.middle}`}>
+          <div className={`${mode?style.child:dark.child}`}>
             <Link href={"/dashboard"} style={{ textDecoration: 'none', color: 'white' }}><MdDashboardCustomize className={style.child_icons} /></Link>
             <Link href={"/dashboard"} style={{ textDecoration: 'none' }}><div className={style.child_link}>Dashboard</div></Link>
           </div>
-          <div className={style.child}>
+          <div className={`${mode?style.child:dark.child}`}>
             <Link href={"/search-form"} style={{ textDecoration: 'none', color: 'white' }}><AiOutlineFileSearch className={style.child_icons} /></Link>
             <Link href={"/search-form"} style={{ textDecoration: 'none' }}><div className={style.child_link}>Search-Form</div></Link>
           </div>
-          <div className={style.child}>
+          <div className={`${mode?style.child:dark.child}`}>
             <Link href={"/analytics"} style={{ textDecoration: 'none', color: 'white' }}><IoMdAnalytics className={style.child_icons} /></Link>
             <Link href={"/analytics"} style={{ textDecoration: 'none' }}><div className={style.child_link}>Analytics</div></Link>
           </div>
         </div>
-        <div className={style.lower}>
-          <div className={style.user_logo}>
+        <div className={`${mode?style.lower:dark.lower}`}>
+          <div className={`${mode?style.user_logo:dark.user_logo}`}>
             <img src={auth_session.user.image} alt={user.name} height={40}/>
           </div>
-          <div className={style.user_info}>
-            <div className={style.user_name}>
+          <div className={`${mode?style.user_info:dark.user_info}`}>
+            <div className={`${mode?style.user_name:dark.user_name}`}>
               <span>{user.username}</span>
             </div>
-            <div className={style.log_out} onClick={() => { signOut() }}>
+            <div className={`${mode?style.log_out:dark.log_out}`} onClick={() => { signOut() }}>
               <span>Logout</span>
             </div>
           </div>
@@ -153,22 +161,28 @@ export default function Dashboard() {
 
 
       {/* ...................................................Right_side .............................................................*/}
-      <div className={style.rightpart}>
-        <div className={style.header}>
-          <Link href={"/"} style={{ textDecoration: 'none' }}><div className={style.content}>Home</div></Link>
-          <div className={style.buttons}>
+      <div className={`${mode?style.rightpart:dark.rightpart}`}>
+        <div className={`${mode?style.header:dark.header}`}>
+          <Link href={"/"} style={{ textDecoration: 'none' }}><div className={`${mode?style.content:dark.content}`}>Home</div></Link>
+          <div className={`${mode?style.buttons:dark.buttons}`}>
             {/* <button className={style.button27} role="button"><Link href={'dashboard/create-form'} className={style.anchor}>+Create Form</Link></button> */}
-            <button className={style.button26} role="button"><Link href={'dashboard/create-quiz'} style={{ textDecoration: 'none', color: 'white' }} className={style.anchor}>+Create Quiz</Link></button>
+            <button className={`${mode?style.button26:dark.button26}`} role="button"><Link href={'dashboard/create-quiz'} style={{ textDecoration: 'none' }} className={`${mode?style.anchor:dark.anchor}`}>+Create Quiz</Link></button>
+            <div className={`${mode?style.iconGrid:dark.iconGrid}`} onClick={modeHandler}>
+              <FaLightbulb className={`${mode?style.bulb:dark.bulb}`}></FaLightbulb>
+            </div>
           </div>
         </div>
-        <div className={style.formscontainer}>
-          <div className={style.sorting} onClick={() => { setAllForm(true); setAllResponse(false) }}>
-            All Forms
+        <div className={`${mode?style.outer:dark.outer}`}>
+        <div className={`${mode?style.formscontainer:dark.formscontainer}`}>
+          <div className={`${mode?style.selector:dark.selector}`}>
+          <div className={`${mode?style.sorting:dark.sorting}`} onClick={() => { setAllForm(true); setAllResponse(false) }}>
+            Forms
           </div>
-          <div className={style.sorting} onClick={() => { setAllForm(false); setAllResponse(true) }}>
-            All Responses
+          <div className={`${mode?style.sorting:dark.sorting}`} onClick={() => { setAllForm(false); setAllResponse(true) }}>
+            Responses
           </div>
-          {!loaderFlag && <div className={style.container}>
+          </div>
+          {!loaderFlag && <div className={`${mode?style.container:dark.container}`}>
             {all_form &&
               data.map(function (val) {
                 return <ResponseCard data={val} key={val._id}></ResponseCard>
@@ -180,6 +194,7 @@ export default function Dashboard() {
               })
             }
           </div>}
+        </div>
           {loaderFlag && <div>
             <Loader></Loader>
           </div>}
--- src/components/ResponseCard.js
+++ src/components/ResponseCard.js
@@ -1,26 +1,49 @@
 import { Avatar } from "@mui/material";
 import style from './ResponseCard.module.css'
+import dark from './darkResponse.module.css'
 import Link from "next/link";
+import { useEffect, useState } from "react";
 const Page=(props)=>{
     // console.log('props',props.data)
+  const [mode,setMode]=useState(true)
+  useEffect(()=>{
+    const modes=()=>{
+      const g=localStorage.getItem('website_mode');
+      if(g===null){
+        setMode(true);
+        localStorage.setItem('website_mode',true);
+      }
+      else{
+        let f=true;
+        if(g==='false'){
+          f=false;
+        }
+        else{
+          f=true
+        }
+        setMode(f)
+      }
+    }
+    modes()
+  })
     const date=new Date(props.data.date);
     var day=date.getDate();
     var month=date.toLocaleString('default', { month: 'long' });
     var year=date.getFullYear();
     var str=`${day} ${month},${year}`
     return <>
-        <div className={style.card1}>
-            <Link href={`/dashboard/${props.data._id}`} className={style.anchor}>
+        <div className={`${mode?style.card1:dark.card1}`}>
+            <Link href={`/dashboard/${props.data._id}`} className={`${mode?style.anchor:dark.anchor}`}>
             <div className={style.header1}>
                 <img src="exam.png" className={style.image}></img>
                 {/* name */}
                 <span>{props.data.form_name}</span>
             </div>
-            <div className={style.msg}>
+            <div className={`${mode?style.msg:dark.msg}`}>
                 {/* date */}
                 <span>Published on<br></br> {str}</span>
             </div>
-            <div className={style.response}>
+            <div className={`${mode?style.response:dark.response}`}>
                 {/* responses */}
                 <span>{props.data.responses_no} Responded</span>
             </div>
