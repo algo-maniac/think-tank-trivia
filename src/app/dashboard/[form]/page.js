4625c68f0d4f08d8909b4f12f02a28f0eeabb56c
Tsaha11 <tuhin727066@gmail.com>
2023-11-12 15:29:34 +0530

    dark mode for dashboard/{id} added completely

--- src/app/dashboard/[form]/page.js
+++ src/app/dashboard/[form]/page.js
@@ -1,5 +1,6 @@
 "use client"
 import style from './style.module.css'
+import dark from './dark.module.css'
 import FormHeader from '../../../components/FormHeader'
 import MCQ from '../../../components/MCQ'
 import Question from '@/components/Question';
@@ -7,8 +8,71 @@ import { useEffect, useState } from 'react';
 import UserContext from '@/context/userContext/userContext';
 import { useContext } from 'react';
 import { useRouter } from 'next/navigation';
+import { FaLightbulb } from 'react-icons/fa'
 export default function Page({ params }) {
     const { auth_status } = useContext(UserContext);
+    const [mode,setMode]=useState(true)
+    let [response, setResponse] = useState([]);
+    let [all_form, setAllForm] = useState(true);
+    let [all_response, setAllResponse] = useState(false);
+    useEffect(()=>{
+        const modes=()=>{
+        const g=localStorage.getItem('website_mode');
+        if(g===null){
+            setMode(true);
+            localStorage.setItem('website_mode',true);
+        }
+        else{
+            let f=true;
+            if(g==='false'){
+            f=false;
+            }
+            else{
+            f=true
+            }
+            setMode(f)
+        }
+        }
+        modes()
+        setAllForm(!all_form);
+        setAllForm(!all_form);
+        setAllResponse(!all_response)
+        setAllResponse(!all_response)
+    },[])
+    useEffect(()=>{
+        const modes=()=>{
+          const g=localStorage.getItem('website_mode');
+          if(g===null){
+            setMode(true);
+            localStorage.setItem('website_mode',true);
+          }
+          else{
+            let f=true;
+            if(g==='false'){
+              f=false;
+            }
+            else{
+              f=true
+            }
+            setMode(f)
+          }
+        }
+        modes()
+        setAllForm(!all_form);
+        setAllForm(!all_form);
+        setAllResponse(!all_response)
+        setAllResponse(!all_response)
+      },[])
+      const modeHandler=()=>{
+        if(mode===true){
+          setMode(false);
+          localStorage.setItem('website_mode',false);
+        }      
+        else{
+          setMode(true);
+          localStorage.setItem('website_mode',true);
+        }
+      }
     const router = useRouter();
     if (auth_status == 'unauthenticated') {
         return router.push('/login');
@@ -28,24 +92,24 @@ export default function Page({ params }) {
         fetchForm();
     }, [])
     return <>
-        <div className={style.header}>
-            <div className={style.logo}>
+        <div className={`${mode?style.header:dark.header}`}>
+            <div className={`${mode?style.logo:dark.logo}`}>
                 <img src="/favicon.png" height="50px"></img>
                 <h3>Think-Fast-Trivia</h3>
             </div>
-            <div className={style.nav}>
+            <div className={`${mode?style.nav:dark.nav}`}>
                 <div><a href={"/dashboard"}>DashBoard</a></div>
                 <div><a href={`/dashboard/${formId}`}>Preview</a></div>
                 <div><a href={`/dashboard/${formId}/share-portal`}>Share</a></div>
                 <div><a href={`/dashboard/${formId}/response`}>Response</a></div>
             </div>
-            <div className={style.mode}>
-                <button className={style.button4}>Light</button>
-                <button className={style.button31}>Dark</button>
+            <div className={`${mode?style.iconGrid:dark.iconGrid}`} onClick={modeHandler}>
+              <FaLightbulb className={`${mode?style.bulb:dark.bulb}`}></FaLightbulb>
             </div>
         </div>
-        <div className={style.contentheader}>
-            <img src="/form-header1.png" height={"100%"} width={"100%"}></img>
+        <div className={`${mode?style.outer:dark.outer}`}>
+        <div className={`${mode?style.contentheader:dark.contentheader}`}>
+            <img src={`${mode?"/form-header1.png":"/nightmode.png"}`} height={"100%"} width={"100%"}></img>
         </div>
         {/* Pass the data by props */}
         <FormHeader formName={formName}></FormHeader>
@@ -59,5 +123,6 @@ export default function Page({ params }) {
                 }
             })
         }
+        </div>
     </>
 }
--- src/components/FormHeader.js
+++ src/components/FormHeader.js
@@ -1,10 +1,40 @@
+import { useEffect, useState } from 'react';
 import style from './FormHeader.module.css'
+import dark from './darkFormHeader.module.css'
 const FormHeader=(props)=>{
+    const [mode,setMode]=useState(true)
+    let [response, setResponse] = useState([]);
+    let [all_form, setAllForm] = useState(true);
+    let [all_response, setAllResponse] = useState(false);
+    useEffect(()=>{
+        const modes=()=>{
+        const g=localStorage.getItem('website_mode');
+        if(g===null){
+            setMode(true);
+            localStorage.setItem('website_mode',true);
+        }
+        else{
+            let f=true;
+            if(g==='false'){
+            f=false;
+            }
+            else{
+            f=true
+            }
+            setMode(f)
+        }
+        }
+        modes()
+        setAllForm(!all_form);
+        setAllForm(!all_form);
+        setAllResponse(!all_response)
+        setAllResponse(!all_response)
+    })
     return<>
-        <div className={style.header}>
-            <h2>{props.formName}</h2>
+        <div className={`${mode?style.header:dark.header}`}>
+            <h2 className={`${mode?style.heading:dark.heading}`}>{props.formName}</h2>
             <hr></hr>
-            <p>This is a form to collect the data from all the student</p>
+            <p className={`${mode?style.msg:dark.msg}`}>This is a form to collect the data from all the student</p>
         </div>
     </>
 }
